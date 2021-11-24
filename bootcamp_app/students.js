a6b8a70ee25ef51928f77cdc745fe7a3ed2e98a2
stew anoya <stewartanoya@gmail.com>
2021-11-24 23:50:37 +0000

    Fix SQL injection vulnerabilities

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,18 +8,23 @@ const pool = new Pool({
 });
 
 const args = process.argv;
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
 pool
-  .query(
-    `SELECT students.id as id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts on cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${args[2]}%'
-LIMIT ${args[3] || 5};`
-  )
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
-        `${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`
+        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
       );
     });
   })
